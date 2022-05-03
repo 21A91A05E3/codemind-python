b514c245b1b20fe980ecaa1d60088d7109f5a8c9
21A91A05E3 <97424858+21A91A05E3@users.noreply.github.com>
2022-05-03 11:37:05 +0530

    n=int(input()) k=ord("A") for i in range(n,0,-1):     for j in range(1,i+1):         print(chr(k+i-1),end=' ')     print( )

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,12 +1,7 @@
 n=int(input())
-for i in range(1,n+1):
-    for j in range(1,n+1):
-        if(i<n):
-            if(j==1 or j==i):
-                print("*",end='')
-            else:
-                print(" ",end='')
-        elif(i==n+1 or j<=n+1):
-            print("*",end='')
-            
+k=ord("A")
+for i in range(n,0,-1):
+    for j in range(1,i+1):
+        print(chr(k+i-1),end=' ')
     print( )
+        
