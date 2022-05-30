783a9c17b3e528b0da533a7e9d06597244585308
21A91A05E3 <97424858+21A91A05E3@users.noreply.github.com>
2022-05-30 18:27:01 +0530

    def p(n):     for i in range(2,int(n**0.5)+1):         if n%i==0:             return 0     else:         return 1 n=int(input()) a=0 b=0 c=0 for i in range(2,n//2+1):     if(p(i)==1 and n%i==0):         a=i         r=n//i         if(p(r)):             c+=1             b=r if(c==0):     print(-1) else:     print(b,a)

---
+++ Find_two_distinct_prime_numbers_with_given_product.py
@@ -0,0 +1,23 @@
+def p(n):
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return 0
+    else:
+        return 1
+n=int(input())
+a=0
+b=0
+c=0
+for i in range(2,n//2+1):
+    if(p(i)==1 and n%i==0):
+        a=i
+        r=n//i
+        if(p(r)):
+            c+=1
+            b=r
+if(c==0):
+    print(-1)
+else:
+    print(b,a)
+
+
