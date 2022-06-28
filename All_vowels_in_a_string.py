61b7fe8f9efa2169b865bce9eaf08aa49b3154c5
21A91A05E3 <97424858+21A91A05E3@users.noreply.github.com>
2022-06-28 19:58:09 +0530

    s=input() s.lower() r="" for i in s:     if i in 'aeiou':         if i not in r:             r=r+i if(len(r)==5):     print("True") else:     print("False")

--- All_vowels_in_a_string.py
+++ All_vowels_in_a_string.py
@@ -1,15 +1,11 @@
 s=input()
-r=set(s)
-s=str(r)
-c1=0
-c2=0
+s.lower()
+r=""
 for i in s:
     if i in 'aeiou':
-        c1+=1
-for i in s:
-    if i in 'AEIOU':
-        c2+=1
-if c1>=5 or c2>=5:
+        if i not in r:
+            r=r+i
+if(len(r)==5):
     print("True")
 else:
     print("False")
