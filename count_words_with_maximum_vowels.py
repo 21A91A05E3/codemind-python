68790c0b6442b775f4a636b96578f819be17ed8b
21A91A05E3 <97424858+21A91A05E3@users.noreply.github.com>
2022-07-14 22:09:51 +0530

    s=input() w=s.split() m=0 vc=0 for i in w:     c=0     for j in i:         if j in 'aeiouAEIOU':             c+=1     if(c>=m):         m=c for i in w:     c=0     for j in i:         if j in 'aeiouAEIOU':             c+=1     if(c==m):         vc+=1 print(vc)

---
+++ count_words_with_maximum_vowels.py
@@ -0,0 +1,19 @@
+s=input()
+w=s.split()
+m=0
+vc=0
+for i in w:
+    c=0
+    for j in i:
+        if j in 'aeiouAEIOU':
+            c+=1
+    if(c>=m):
+        m=c
+for i in w:
+    c=0
+    for j in i:
+        if j in 'aeiouAEIOU':
+            c+=1
+    if(c==m):
+        vc+=1
+print(vc)
