f44cb6fc5d45d8484010ecbe929ca22ab4e04ec0
21A91A05E3 <97424858+21A91A05E3@users.noreply.github.com>
2022-07-15 19:22:13 +0530

    n=int(input()) l=list(map(int,input().split())) s=0 c=0 for i in l:     s=s+i r=s//n for j in l:     if j==r:         c+=1         break if(c):     print("True") else:     print("False")

---
+++ average_element_is_in_array_or_not.py
@@ -0,0 +1,15 @@
+n=int(input())
+l=list(map(int,input().split()))
+s=0
+c=0
+for i in l:
+    s=s+i
+r=s//n
+for j in l:
+    if j==r:
+        c+=1
+        break
+if(c):
+    print("True")
+else:
+    print("False")
