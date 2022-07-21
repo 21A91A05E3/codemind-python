1d6fead079c2e06690f0fd99a6cf77f10b00f6b1
21A91A05E3 <97424858+21A91A05E3@users.noreply.github.com>
2022-07-21 19:57:48 +0530

    n=int(input()) for i in range(1,n+1):     for j in range(1,n-i+1):         print(end=" ")     for j in range(i,0,-1):         print(i,end="")     for j in range(2,i+1):         print(i,end="")     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,7 +1,11 @@
 n=int(input())
-k=ord("A")
-for i in range(n,0,-1):
-    for j in range(1,i+1):
-        print(chr(k+i-1),end=' ')
-    print( )
-        
+for i in range(1,n+1):
+    for j in range(1,n-i+1):
+        print(end=" ")
+    for j in range(i,0,-1):
+        print(i,end="")
+    for j in range(2,i+1):
+        print(i,end="")
+    print()
+
+
