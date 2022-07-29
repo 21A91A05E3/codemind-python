f63c2f7481fa19062f49ee2ac74fa90d01e4c199
21A91A05E3 <97424858+21A91A05E3@users.noreply.github.com>
2022-07-29 21:26:45 +0530

    s1=input() s2=input() if len(s1)<=len(s2):     s1=s1.lower() else:     s2=s2.lower() r="" for i in s1:     if i!=" ":         if i in s2:             if i not in r:                 r=r+i if len(r):     print("".join(sorted(r))) else:     print("-1")

---
+++ common_characters.py
@@ -0,0 +1,16 @@
+s1=input()
+s2=input()
+if len(s1)<=len(s2):
+    s1=s1.lower()
+else:
+    s2=s2.lower()
+r=""
+for i in s1:
+    if i!=" ":
+        if i in s2:
+            if i not in r:
+                r=r+i
+if len(r):
+    print("".join(sorted(r)))
+else:
+    print("-1")
