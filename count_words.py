8a8830567d1c51f3b038fa23d0d7f047efe833bb
21A91A05E3 <97424858+21A91A05E3@users.noreply.github.com>
2022-07-30 19:40:36 +0530

    s=input() w=s.split() c=0 for i in w:     f=i[0]     l=i[-1]     if f in 'AEIOUaeiou':         if l not in 'AEIOUaeiou':             c+=1 print(c)

--- count_words.py
+++ count_words.py
@@ -1,8 +1,10 @@
 s=input()
 w=s.split()
 c=0
-for word in w:
-    if word[0] in 'AEIOUaeiou':
-        if word[-1] not in 'AEIOUaeiou':
+for i in w:
+    f=i[0]
+    l=i[-1]
+    if f in 'AEIOUaeiou':
+        if l not in 'AEIOUaeiou':
             c+=1
 print(c)
