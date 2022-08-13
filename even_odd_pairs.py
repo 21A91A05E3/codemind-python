a0967310cb61e1ab3c97badc77a2ce931366e091
21A91A05E3 <97424858+21A91A05E3@users.noreply.github.com>
2022-08-13 10:07:02 +0530

    n=int(input()) l=list(map(int,input().split())) e=[] o=[] for i in l:     if i%2==0:         e.append(i)     else:         o.append(i) i,j=0,0 l1=[] while(i<len(e) and j<len(o)):     l1.append(e[i])     i+=1     l1.append(o[j])     j+=1 while(i<len(e)):     l1.append(e[i])     i+=1 while(j<len(o)):     l1.append(o[j])     j+=1 if len(l1)%2!=0:     l1.append("0") print(*l1)

---
+++ even_odd_pairs.py
@@ -0,0 +1,28 @@
+n=int(input())
+l=list(map(int,input().split()))
+e=[]
+o=[]
+for i in l:
+    if i%2==0:
+        e.append(i)
+    else:
+        o.append(i)
+i,j=0,0
+l1=[]
+while(i<len(e) and j<len(o)):
+    l1.append(e[i])
+    i+=1
+    l1.append(o[j])
+    j+=1
+while(i<len(e)):
+    l1.append(e[i])
+    i+=1
+while(j<len(o)):
+    l1.append(o[j])
+    j+=1
+if len(l1)%2!=0:
+    l1.append("0")
+print(*l1)
+
+    
+    
