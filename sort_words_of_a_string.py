45a66d350cdeacf38180c35c628fa2c61a0b5de7
21A91A05E3 <97424858+21A91A05E3@users.noreply.github.com>
2022-08-23 13:18:25 +0530

    s=input() w=s.split() r=[] for i in w:     v=""     c=""     l=""     for j in i:         if j.isalpha():             v=v+j         else:             c=c+j     v=sorted(v)     k=0     p=0     for j in i:         if j.isalpha():             l=l+v[k]             k+=1         else:             l=l+c[p]             p+=1     r.append(l) for i in r:     print(i,end=" ")

---
+++ sort_words_of_a_string.py
@@ -0,0 +1,28 @@
+s=input()
+w=s.split()
+r=[]
+for i in w:
+    v=""
+    c=""
+    l=""
+    for j in i:
+        if j.isalpha():
+            v=v+j
+        else:
+            c=c+j
+    v=sorted(v)
+    k=0
+    p=0
+    for j in i:
+        if j.isalpha():
+            l=l+v[k]
+            k+=1
+        else:
+            l=l+c[p]
+            p+=1
+    r.append(l)
+for i in r:
+    print(i,end=" ")
+
+
+    
