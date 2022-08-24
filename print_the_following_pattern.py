7f63ebee72931378f44be89df8e64f392667526b
21A91A05E3 <97424858+21A91A05E3@users.noreply.github.com>
2022-08-24 10:21:53 +0530

    r=int(input()) for i in range (1,r+1):     for j in range (1, r-i + 1):         print (end=" ")     if i == 1 or i == r:         for j in range (1, r+ 1):             print ("*",end="")     else:         for j in range (1,r+1):             if (j == 1 or j == r):                 print ("*",end="")             else:                 print (end=" ")     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,9 +1,14 @@
-n=int(input())
-for i in range(1,n+1):
-    for j in range(1,n-i+1):
-        print(end=" ")
-    for j in range(i,0,-1):
-        print(j-1,end="")
-    for j in range(2,i+1):
-        print(j-1,end="")
+r=int(input())
+for i in range (1,r+1):
+    for j in range (1, r-i + 1):
+        print (end=" ")
+    if i == 1 or i == r:
+        for j in range (1, r+ 1):
+            print ("*",end="")
+    else:
+        for j in range (1,r+1):
+            if (j == 1 or j == r):
+                print ("*",end="")
+            else:
+                print (end=" ")
     print()
